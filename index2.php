0d0a9e079f122b7a0c898f8c0edb1251839e7e49
codafrica2019 <46863394+codafrica2019@users.noreply.github.com>
2019-01-20 11:20:44 +0000

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" lang="fr"/>
+      <meta name="robots" content="index, follow"/>
+      <meta name="renderer" content="webkit"/>
+      <meta name="keywords" content=" "/> 
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes"/>
+      <link rel="stylesheet" media="all" href="squelette_index.css"/>
+      <link rel="icon" type="image/png" href=" "/>
+      <title>Accueil - codafric</title>
+   </head>
+   
+   <body id="body">
+      
+      <!-- Nav bar contenant Logo + moteur de recherche + menu -->
+      <nav id="principal_top_nav_fixed">
+       <center><p>logo et plus </p></center>
+      </nav>
+      
+      <!-- La légende du site -->
+      <div id="legeng_codafric">
+      
+      <center><p> légende</p></center>
+      
+      </div>
+      
+      <!-- Slider automatique-->
+      <div id="big_top_Slider_auto">
+      
+      <center><p>Slider</p></center>
+      
+      </div>
+      
+      <!-- Conteneur principal de la page -->
+      <div id="principal_center_body">
+         
+         <!-- marge -->
+         <div id="margin_option_in_left">
+         
+         <center><p> marge de page </p></center>
+          
+         </div>
+         
+         <!-- body -->
+         <div id="body_in_left">
+         body
+         </div>
+      </div>
+      
+      <!--footer contenant informations légaux, etc..-->
+      <footer id="responsible_footer">
+      
+         <center><p>Pied de page000000000
+		 0
+		 </p></center>
+      </footer>
+      
+   </body>
+</html>
